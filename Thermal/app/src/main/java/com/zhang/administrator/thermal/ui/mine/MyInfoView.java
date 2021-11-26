2258c3144765102cc4144b9b28ee4df9896a2a92
Zhang ShuoFeng <852333743@qq.com>
2021-11-26 23:22:24 +0800

    清除缓存

--- Thermal/app/src/main/java/com/zhang/administrator/thermal/ui/mine/MyInfoView.java
+++ Thermal/app/src/main/java/com/zhang/administrator/thermal/ui/mine/MyInfoView.java
@@ -2,8 +2,10 @@ package com.zhang.administrator.thermal.ui.mine;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,6 +15,11 @@ import android.widget.Toast;
 
 import com.zhang.administrator.thermal.R;
 import com.zhang.administrator.thermal.util.AnalysisUtils;
+
+import java.io.File;
+import java.math.BigDecimal;
+
+import androidx.appcompat.app.AlertDialog;
 
 /**
  * Created by EWorld
@@ -29,6 +36,8 @@ public class MyInfoView {
     private RelativeLayout mSetting;
     private RelativeLayout mCleanCache;
     private RelativeLayout mAbout;
+
+    private TextView mCacheTv;
 
     public MyInfoView(Activity activity) {
         this.activity = activity;
@@ -48,11 +57,12 @@ public class MyInfoView {
         mSetting = mView.findViewById(R.id.rl_mine_setting);
         mCleanCache = mView.findViewById(R.id.rl_mine_clean_cache);
         mAbout = mView.findViewById(R.id.rl_mine_about);
-
+        mCacheTv = mView.findViewById(R.id.tv_cache);
         mView.setVisibility(View.VISIBLE);
 
         initEvent();
         setLoginParams(readLoginStatus());
+        setCacheSize();
     }
 
     private void initEvent() {
@@ -94,11 +104,101 @@ public class MyInfoView {
         mCleanCache.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showCacheDialog();
             }
         });
 
         mAbout.setOnClickListener(v -> AboutProductActivity.startActivity(activity));
+    }
+
+    private String getTotalCacheSize() {
+        long cacheSize = getFolderSize(activity.getCacheDir());
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            cacheSize += getFolderSize(activity.getExternalCacheDir());
+        }
+        return getFormatSize(cacheSize);
+    }
+
+    private long getFolderSize(File file) {
+        long size = 0;
+        File[] files = file.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                size = size + getFolderSize(files[i]);
+            } else {
+                size = size + files[i].length();
+            }
+        }
+        return size;
+    }
+
+    private String getFormatSize(double size) {
+        double kiloByte = size / 1024;
+        if (kiloByte < 1) {
+            return "0KB";
+        }
+        double megaByte = kiloByte / 1024;
+        if (megaByte < 1) {
+            BigDecimal result_1 = new BigDecimal(Double.toString(kiloByte));
+            return result_1.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "KB";
+        }
+
+        double gigaByte = megaByte / 1024;
+        if (gigaByte < 1) {
+            BigDecimal result_2 = new BigDecimal(Double.toString(megaByte));
+            return result_2.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "MB";
+        }
+
+        double teraByte = gigaByte / 1024;
+        if (teraByte < 1) {
+            BigDecimal result_3 = new BigDecimal(Double.toString(gigaByte));
+            return result_3.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "GB";
+        }
+
+        BigDecimal result_4 = new BigDecimal(Double.toString(teraByte));
+        return result_4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "TB";
+    }
+
+    private void showCacheDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("提醒！");
+        builder.setMessage("是否清空缓存");
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                clearAllCacheData();
+                mCacheTv.setText("0KB");
+
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void clearAllCacheData() {
+        deleteDir(activity.getCacheDir());
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            deleteDir(activity.getExternalCacheDir());
+        }
+    }
+
+    private boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
     }
 
     private void openPersonView() {
@@ -123,6 +223,10 @@ public class MyInfoView {
         } else {
             mUserNameTv.setText("点击登录");
         }
+    }
+
+    public void setCacheSize() {
+        mCacheTv.setText(getTotalCacheSize());
     }
 
     public View getView() {
